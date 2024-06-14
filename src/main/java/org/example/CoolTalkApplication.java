6e270f953a30c28eef773d9fa0fa7cf4df0ffcdc
Bread65 <2334557728@qq.com>
2024-06-14 15:10:31 +0800

    第一次提交

---
+++ src/main/java/org/example/CoolTalkApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class CoolTalkApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoolTalkApplication.class, args);
+    }
+}
