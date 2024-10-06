28d23a58d953a4d82c4341f1b812ef75a0222ffd
DESKTOP-0PGGURA\YXF <2334557728@qq.com>
2024-10-06 22:04:59 +0800

    增加swaggerUI和改写相关依赖

--- src/main/java/com/yxf/CoolTalk/controller/UserController.java
+++ src/main/java/com/yxf/CoolTalk/controller/UserController.java
@@ -1,19 +1,22 @@
 package com.yxf.CoolTalk.controller;
 
 import com.yxf.CoolTalk.common.result.CommonResult;
+import com.yxf.CoolTalk.config.RedisConfig;
 import com.yxf.CoolTalk.mbg.model.CooltalkUser;
 import com.yxf.CoolTalk.service.UserService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
 
+@Slf4j
 @Api(tags="UserController")
 @CrossOrigin(origins = "http://localhost:5173/")
 @RestController
@@ -21,6 +24,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserController {
     @Autowired
     private UserService userService;
+
+    @ApiOperation("查询所有用户")
+    @GetMapping("/list")
+    public CommonResult<List<CooltalkUser>> listUsers(){
+        List<CooltalkUser> data=userService.listUsers();
+        return CommonResult.success(data);
+    }
+
+    @ApiOperation("根据账户名查询用户")
+    @GetMapping("/User/{account}")
+    public CommonResult<CooltalkUser> getUserByAccount(@PathVariable("account") @ApiParam("账户名")String account){
+        CooltalkUser data=userService.getUserByAccount(account);
+        return CommonResult.success(data);
+    }
 
     @ApiOperation("登录")
     @PostMapping("/login")
@@ -35,12 +52,23 @@ public class UserController {
     @ApiOperation("注册用户")
     @PostMapping("/user")
     public CommonResult<Void> createUser(@RequestBody @ApiParam("注册用户") CooltalkUser user){
-        int res=userService.createUser(user);
-        return switch (res) {
-            case (-1) -> CommonResult.fail("用户名已存在");
-            case (0) -> CommonResult.fail("用户创建失败");
-            case (1) -> CommonResult.success(null);
-            default -> CommonResult.fail("发生未知错误");
-        };
+        log.info("注册时，用户性别为"+user.getGender());
+        CooltalkUser res=userService.createUser(user);
+        log.info("注册后，用户性别为"+user.getGender());
+        return res!=null?CommonResult.success(null):CommonResult.fail("用户创建失败");
+    }
+
+    @ApiOperation("更新用户")
+    @PutMapping("/updateUser")
+    public CommonResult<Void> updateUser(@RequestBody @ApiParam("更新用户信息") CooltalkUser user){
+        int data=userService.updateUser(user);
+        return data>0?CommonResult.success(null):CommonResult.fail("更新用户失败");
+    }
+
+    @ApiOperation("删除用户")
+    @DeleteMapping("/deleteUser")
+    public CommonResult<Void> deleteUser(@RequestBody @ApiParam("删除用户账号") String account){
+        int data=userService.deleteUser(account);
+        return data>0?CommonResult.success(null):CommonResult.fail("删除用户失败");
     }
 }
